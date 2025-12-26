5d4e6fac92151c7053346dc155a892f5c7dfb9a5
Bipul kumar <bipulkumar@Bipuls-Mac-mini.local>
2025-12-26 12:21:30 +0530

    Update favicons and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -69,6 +69,9 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
+      { url: "/favicon-16.png", type: "image/png", sizes: "16x16" },
+      { url: "/favicon-32.png", type: "image/png", sizes: "32x32" },
+      { url: "/favicon-48.png", type: "image/png", sizes: "48x48" },
       { url: "/icon-192.png", type: "image/png", sizes: "192x192" },
       { url: "/icon-512.png", type: "image/png", sizes: "512x512" }
     ],
